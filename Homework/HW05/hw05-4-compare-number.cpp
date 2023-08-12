8f3f1cf8efb53f3d68bbbe402a2d2bb28d8603c9
Mekha1104 <gtrcooie@gmail.com>
2023-08-12 15:51:43 +0700

    05-4

--- Homework/HW05/hw05-4-compare-number.cpp
+++ Homework/HW05/hw05-4-compare-number.cpp
@@ -30,10 +30,10 @@
 int main() {
     int num1, num2;
 
-    printf("Input 1 : ");
+    printf("Input 1 :\n ");
     scanf("%d", &num1);
 
-    printf("Input 2 : ");
+    printf("Input 2 :\n ");
     scanf("%d", &num2);
 
     if (num1 < num2) {
